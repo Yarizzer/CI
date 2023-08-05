742ed76cfa06f287ea4248784045b47ae239d58d
Yaroslav Abaturov <yarizzer@gmail.com>
2023-08-05 20:45:58 +0200

    minor changes

--- CI/Common/Providers/TableViewProvider/Protocols/TableViewProviderType.swift
+++ CI/Common/Providers/TableViewProvider/Protocols/TableViewProviderType.swift
@@ -9,7 +9,7 @@ import UIKit
 
 protocol TableViewProviderType {
     var onConfigureCell: ((IndexPath) -> UITableViewCell)? { get set }
-    var onSelectCell: ((IndexPath) -> Void)? { get set }
+    var onSelectCell: ((IndexPath) -> ())? { get set }
     func registerCells(_ cells: [UITableViewCell.Type])
     func dequeueReusableCell<T>(for indexPath: IndexPath) -> T where T: UITableViewCell
     func reloadData()
