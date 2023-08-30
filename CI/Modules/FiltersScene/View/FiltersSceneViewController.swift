aefc722a0358fb3d1d4fc72d99a3140cefac6a89
Yaroslav Abaturov <yarizzer@gmail.com>
2023-08-30 20:16:47 +0200

    minor ch

--- CI/Modules/FiltersScene/View/FiltersSceneViewController.swift
+++ CI/Modules/FiltersScene/View/FiltersSceneViewController.swift
@@ -9,112 +9,112 @@
 import UIKit
 
 class FiltersSceneViewController: BaseViewController<FiltersSceneInteractable> {
-	// MARK: View lifecycle
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		setup()
-	}
+  // MARK: View lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        view.layoutIfNeeded()
-        UIView.animate(withDuration: Constants.initialAD, delay: 0, options: [.curveEaseOut, .allowUserInteraction], animations: extractSelf { sSelf in
-            sSelf.topPaddingConstraint.constant -= Constants.topConstraintExtraValue
-            sSelf.view.layoutIfNeeded()
-        }, completion: extractSelf { sSelf, _ in
-            sSelf.backButton.appear()
-            
-            UIView.animate(withDuration: Constants.initialAD, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.7, options: [.curveEaseOut, .allowUserInteraction], animations: sSelf.extractSelf { sSelf in
-                sSelf.popUpMenuTopPaddingConstraint.constant = Constants.popUpTopConstraintDefaultValue
-                sSelf.view.layoutIfNeeded()
-            }, completion: sSelf.extractSelf { sSelf, _ in
-                sSelf.interactor?.makeRequest(requestType: .viewIsReady)
-            })
-        })
-    }
+    setup()
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
     
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        
-        topPaddingConstraint.constant = AppCore.shared.uiLayer.device.topPaddingValue + Constants.topConstraintExtraValue
-    }
-	
-	private func setup() {
-        topPaddingConstraint.constant = AppCore.shared.uiLayer.device.topPaddingValue + Constants.topConstraintExtraValue
-        
-		interactor?.makeRequest(requestType: .initialSetup)
-	}
+    view.layoutIfNeeded()
+    UIView.animate(withDuration: Constants.initialAD, delay: 0, options: [.curveEaseOut, .allowUserInteraction], animations: extractSelf { sSelf in
+      sSelf.topPaddingConstraint.constant -= Constants.topConstraintExtraValue
+      sSelf.view.layoutIfNeeded()
+    }, completion: extractSelf { sSelf, _ in
+      sSelf.backButton.appear()
+      
+      UIView.animate(withDuration: Constants.initialAD, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.7, options: [.curveEaseOut, .allowUserInteraction], animations: sSelf.extractSelf { sSelf in
+        sSelf.popUpMenuTopPaddingConstraint.constant = Constants.popUpTopConstraintDefaultValue
+        sSelf.view.layoutIfNeeded()
+      }, completion: sSelf.extractSelf { sSelf, _ in
+        sSelf.interactor?.makeRequest(requestType: .viewIsReady)
+      })
+    })
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
     
-    @IBAction private func backButtonAction(_ sender: BackButton) {
-        interactor?.makeRequest(requestType: .routeBack)
-    }
+    topPaddingConstraint.constant = AppCore.shared.uiLayer.device.topPaddingValue + Constants.topConstraintExtraValue
+  }
+  
+  private func setup() {
+    topPaddingConstraint.constant = AppCore.shared.uiLayer.device.topPaddingValue + Constants.topConstraintExtraValue
     
-    @IBOutlet private weak var topPaddingConstraint: NSLayoutConstraint!
-    @IBOutlet private weak var popUpMenuTopPaddingConstraint: NSLayoutConstraint!
-    
-    @IBOutlet private weak var backButton: BackButton!
-    @IBOutlet private weak var sceneTitle: UILabel!
-    @IBOutlet private weak var popUpMenu: FiltersScenePopUpMenu!
-    @IBOutlet private weak var imageView: UIImageView!
-    
+    interactor?.makeRequest(requestType: .initialSetup)
+  }
+  
+  @IBAction private func backButtonAction(_ sender: BackButton) {
+    interactor?.makeRequest(requestType: .routeBack)
+  }
+  
+  @IBOutlet private weak var topPaddingConstraint: NSLayoutConstraint!
+  @IBOutlet private weak var popUpMenuTopPaddingConstraint: NSLayoutConstraint!
+  
+  @IBOutlet private weak var backButton: BackButton!
+  @IBOutlet private weak var sceneTitle: UILabel!
+  @IBOutlet private weak var popUpMenu: FiltersScenePopUpMenu!
+  @IBOutlet private weak var imageView: UIImageView!
+  
 }
 
 extension FiltersSceneViewController: FiltersSceneViewControllerType {
-	func update(viewModelDataType: FiltersSceneViewControllerViewModel.ViewModelDataType) {
-		switch viewModelDataType {
-		case .initialSetup(let model):
-            sceneTitle.text = model.initialSceneTitle
-            
-            popUpMenu.setup(with: model.getPopUpMenuViewModel())
-        case .needToHidePopUpMenu(let value):
-            guard let value else { return }
-            
-            view.layoutIfNeeded()
-            UIView.animate(withDuration: Constants.popUpMenuAD, delay: 0, options: [.curveEaseInOut, .allowUserInteraction],animations: extractSelf { sSelf in
-                sSelf.popUpMenuTopPaddingConstraint.constant = value ? Constants.popUpTopConstraintDefaultValue : Constants.popUpTopConstraintExtendedValue
-                
-                sSelf.view.layoutIfNeeded()
-            })
-        case .updateTitle(let data):
-            guard let data else { return }
-            
-            UIView.animate(withDuration: Constants.titleAD / 2, delay: 0, options: [.allowUserInteraction], animations: extractSelf { sSelf in
-                sSelf.sceneTitle.alpha = Constants.alpha.min
-            }, completion: extractSelf { sSelf, finished in
-                sSelf.sceneTitle.text = data
-                UIView.animate(withDuration: Constants.titleAD, delay: 0, options: [.allowUserInteraction], animations: sSelf.extractSelf { sSelf in
-                    sSelf.sceneTitle.alpha = Constants.alpha.max
-                })
-            })
-        case .updateImage(let imageData):
-            guard let imageData, let image = UIImage(data: imageData) else { return }
-            
-            UIView.animate(withDuration: Constants.imageUpdateAD / 2, delay: 0, options: [.allowUserInteraction], animations: extractSelf { sSelf in
-                sSelf.imageView.alpha = Constants.alpha.min
-            }, completion: extractSelf { sSelf, finished in
-                sSelf.imageView.image = image
-                UIView.animate(withDuration: Constants.imageUpdateAD, delay: 0, options: [.allowUserInteraction], animations: sSelf.extractSelf { sSelf in
-                    sSelf.imageView.alpha = Constants.alpha.max
-                })
-            })
-		}
-	}
+  func update(viewModelDataType: FiltersSceneViewControllerViewModel.ViewModelDataType) {
+    switch viewModelDataType {
+    case .initialSetup(let model):
+      sceneTitle.text = model.initialSceneTitle
+      
+      popUpMenu.setup(with: model.getPopUpMenuViewModel())
+    case .needToHidePopUpMenu(let value):
+      guard let value else { return }
+      
+      view.layoutIfNeeded()
+      UIView.animate(withDuration: Constants.popUpMenuAD, delay: 0, options: [.curveEaseInOut, .allowUserInteraction],animations: extractSelf { sSelf in
+        sSelf.popUpMenuTopPaddingConstraint.constant = value ? Constants.popUpTopConstraintDefaultValue : Constants.popUpTopConstraintExtendedValue
+        
+        sSelf.view.layoutIfNeeded()
+      })
+    case .updateTitle(let data):
+      guard let data else { return }
+      
+      UIView.animate(withDuration: Constants.titleAD / 2, delay: 0, options: [.allowUserInteraction], animations: extractSelf { sSelf in
+        sSelf.sceneTitle.alpha = Constants.alpha.min
+      }, completion: extractSelf { sSelf, finished in
+        sSelf.sceneTitle.text = data
+        UIView.animate(withDuration: Constants.titleAD, delay: 0, options: [.allowUserInteraction], animations: sSelf.extractSelf { sSelf in
+          sSelf.sceneTitle.alpha = Constants.alpha.max
+        })
+      })
+    case .updateImage(let imageData):
+      guard let imageData, let image = UIImage(data: imageData) else { return }
+      
+      UIView.animate(withDuration: Constants.imageUpdateAD / 2, delay: 0, options: [.allowUserInteraction], animations: extractSelf { sSelf in
+        sSelf.imageView.alpha = Constants.alpha.min
+      }, completion: extractSelf { sSelf, finished in
+        sSelf.imageView.image = image
+        UIView.animate(withDuration: Constants.imageUpdateAD, delay: 0, options: [.allowUserInteraction], animations: sSelf.extractSelf { sSelf in
+          sSelf.imageView.alpha = Constants.alpha.max
+        })
+      })
+    }
+  }
 }
 
 extension FiltersSceneViewController {
-	private struct Constants {
-        //Alpha
-        static let alpha: (min: CGFloat, max: CGFloat) = (min: 0.0, max: 1.0)
-        //Animation duration
-        static let initialAD: Double = 0.5
-        static let popUpMenuAD: Double = 0.5
-        static let titleAD: Double = 0.2
-        static let imageUpdateAD: Double = 1.0
-        //Constraints
-        static let topConstraintExtraValue: CGFloat = 30.0
-        static let popUpTopConstraintDefaultValue: CGFloat = -40.0
-        static let popUpTopConstraintExtendedValue: CGFloat = -AppCore.shared.uiLayer.device.screenSize.height / 2
-	}
+  private struct Constants {
+    //Alpha
+    static let alpha: (min: CGFloat, max: CGFloat) = (min: 0.0, max: 1.0)
+    //Animation duration
+    static let initialAD: Double = 0.5
+    static let popUpMenuAD: Double = 0.5
+    static let titleAD: Double = 0.2
+    static let imageUpdateAD: Double = 1.0
+    //Constraints
+    static let topConstraintExtraValue: CGFloat = 30.0
+    static let popUpTopConstraintDefaultValue: CGFloat = -30.0
+    static let popUpTopConstraintExtendedValue: CGFloat = -AppCore.shared.uiLayer.device.screenSize.height / 2
+  }
 }
--- CI/Modules/MainScene/View/MainSceneViewController.swift
+++ CI/Modules/MainScene/View/MainSceneViewController.swift
@@ -9,108 +9,108 @@
 import UIKit
 
 class MainSceneViewController: BaseViewController<MainSceneInteractable> {
-	// MARK: View lifecycle
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		setup()
-	}
+  // MARK: View lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        view.layoutIfNeeded()
-        UIView.animate(withDuration: Constants.initialAD, delay: 0, options: [.curveEaseOut, .allowUserInteraction], animations: extractSelf { sSelf in
-            sSelf.topPaddingConstraint.constant -= Constants.topConstraintExtraValue
-            sSelf.view.layoutIfNeeded()
-        }, completion: extractSelf { sSelf, _ in
-            sSelf.interactor?.makeRequest(requestType: .viewIsReady)
-        })
-    }
+    setup()
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
     
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        
-        topPaddingConstraint.constant = AppCore.shared.uiLayer.device.topPaddingValue + Constants.topConstraintExtraValue
-        
-        tableView.alpha = Constants.alpha.min
-    }
-	
-	private func setup() {
-        topPaddingConstraint.constant = AppCore.shared.uiLayer.device.topPaddingValue + Constants.topConstraintExtraValue
-        
-        tableView.tableFooterView = UIView(frame: .zero)
-        
-		interactor?.makeRequest(requestType: .initialSetup)
-	}
+    view.layoutIfNeeded()
+    UIView.animate(withDuration: Constants.initialAD, delay: 0, options: [.curveEaseOut, .allowUserInteraction], animations: extractSelf { sSelf in
+      sSelf.topPaddingConstraint.constant -= Constants.topConstraintExtraValue
+      sSelf.view.layoutIfNeeded()
+    }, completion: extractSelf { sSelf, _ in
+      sSelf.interactor?.makeRequest(requestType: .viewIsReady)
+    })
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
     
-    private var provider: TableViewProviderType?
+    topPaddingConstraint.constant = AppCore.shared.uiLayer.device.topPaddingValue + Constants.topConstraintExtraValue
     
-    @IBOutlet private weak var topPaddingConstraint: NSLayoutConstraint!
-    @IBOutlet private weak var tableView: UITableView!
+    tableView.alpha = Constants.alpha.min
+  }
+  
+  private func setup() {
+    topPaddingConstraint.constant = AppCore.shared.uiLayer.device.topPaddingValue + Constants.topConstraintExtraValue
+    
+    tableView.tableFooterView = UIView(frame: .zero)
+    
+    interactor?.makeRequest(requestType: .initialSetup)
+  }
+  
+  private var provider: TableViewProviderType?
+  
+  @IBOutlet private weak var topPaddingConstraint: NSLayoutConstraint!
+  @IBOutlet private weak var tableView: UITableView!
 }
 
 extension MainSceneViewController: MainSceneViewControllerType {
-	func update(viewModelDataType: MainSceneViewControllerViewModel.ViewModelDataType) {
-		switch viewModelDataType {
-		case .initialSetup(let model):
-			print("\(self) \(#function) with model instance \(model)")
-        case .setupProvider(let model):
-            guard let presenterModel = model as? TableViewProviderViewModel else { return }
-            
-            provider = TableViewProvider(for: self.tableView, with: presenterModel)
-            provider?.registerCells([MainSceneFiltersCell.self])
-            
-            provider?.onConfigureCell = { [weak self] indexPath in
-                guard let provider = self?.provider else { return UITableViewCell() }
-
-                let cellModel = model.getCellViewModel(with: indexPath.row)
-
-                switch cellModel.type {
-                case .filters:
-                    let tableViewCell: MainSceneFiltersCell = provider.dequeueReusableCell(for: indexPath)
-
-                    tableViewCell.setup(with: cellModel)
-
-                    return tableViewCell
-                }
-            }
-
-            provider?.onSelectCell = extractSelf { sSelf, indexPath in
-                AppCore.shared.uiLayer.device.generateFeedback(with: .success)
-                sSelf.interactor?.makeRequest(requestType: .itemSelected(withIndex: indexPath.row))
-            }
-        case .reloadProvider:
-            UIView.animate(withDuration: Constants.tableViewAD / 2, delay: 0, options: [.allowUserInteraction], animations: extractSelf { sSelf in
-                sSelf.tableView.alpha = Constants.alpha.min
-            }, completion: extractSelf { sSelf, _ in
-                sSelf.provider?.reloadData()
-                UIView.animate(withDuration: Constants.tableViewAD / 2, delay: 0, options: [.allowUserInteraction], animations: sSelf.extractSelf { sSelf in
-                    sSelf.tableView.alpha = Constants.alpha.max
-                })
-            })
-		}
-	}
+  func update(viewModelDataType: MainSceneViewControllerViewModel.ViewModelDataType) {
+    switch viewModelDataType {
+    case .initialSetup(let model):
+      print("\(self) \(#function) with model instance \(model)")
+    case .setupProvider(let model):
+      guard let presenterModel = model as? TableViewProviderViewModel else { return }
+      
+      provider = TableViewProvider(for: self.tableView, with: presenterModel)
+      provider?.registerCells([MainSceneFiltersCell.self])
+      
+      provider?.onConfigureCell = { [weak self] indexPath in
+        guard let provider = self?.provider else { return UITableViewCell() }
+        
+        let cellModel = model.getCellViewModel(with: indexPath.row)
+        
+        switch cellModel.type {
+        case .filters:
+          let tableViewCell: MainSceneFiltersCell = provider.dequeueReusableCell(for: indexPath)
+          
+          tableViewCell.setup(with: cellModel)
+          
+          return tableViewCell
+        }
+      }
+      
+      provider?.onSelectCell = extractSelf { sSelf, indexPath in
+        AppCore.shared.uiLayer.device.generateFeedback(with: .success)
+        sSelf.interactor?.makeRequest(requestType: .itemSelected(withIndex: indexPath.row))
+      }
+    case .reloadProvider:
+      UIView.animate(withDuration: Constants.tableViewAD / 2, delay: 0, options: [.allowUserInteraction], animations: extractSelf { sSelf in
+        sSelf.tableView.alpha = Constants.alpha.min
+      }, completion: extractSelf { sSelf, _ in
+        sSelf.provider?.reloadData()
+        UIView.animate(withDuration: Constants.tableViewAD / 2, delay: 0, options: [.allowUserInteraction], animations: sSelf.extractSelf { sSelf in
+          sSelf.tableView.alpha = Constants.alpha.max
+        })
+      })
+    }
+  }
 }
 
 extension MainSceneViewController: UIViewControllerTransitioningDelegate {
-    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        BlurTransitionAnimator()
-    }
-    
-    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        BlurTransitionAnimator()
-    }
+  func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    BlurTransitionAnimator()
+  }
+  
+  func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    BlurTransitionAnimator()
+  }
 }
 
 extension MainSceneViewController {
-	private struct Constants {
-        //Alpha
-        static let alpha: (min: CGFloat, max: CGFloat) = (min: 0.0, max: 1.0)
-        //Animation duration
-        static let initialAD: Double = 0.5
+  private struct Constants {
+    //Alpha
+    static let alpha: (min: CGFloat, max: CGFloat) = (min: 0.0, max: 1.0)
+    //Animation duration
+    static let initialAD: Double = 0.5
     static let tableViewAD: Double = 1
-        //Constraints
-        static let topConstraintExtraValue: CGFloat = 30.0
-	}
+    //Constraints
+    static let topConstraintExtraValue: CGFloat = 30.0
+  }
 }
