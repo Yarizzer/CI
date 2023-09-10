7664da1baffb1cff683a3f6468263a9b82e2064b
Yaroslav Abaturov <yarizzer@gmail.com>
2023-09-10 08:19:06 +0200

    PickerViewProviderViewModel ch

--- CI/Common/Providers/PickerView/Protocols/PickerViewProviderViewModel.swift
+++ CI/Common/Providers/PickerView/Protocols/PickerViewProviderViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Yaroslav Abaturov on 26.07.2023.
 //
 
-protocol PickerViewProviderViewModel {
+protocol PickerViewProviderViewModel: AnyObject {
     var numberOfComponents: Int { get }
     func numberOfRowsIn(component: Int) -> Int
     func rowHeight(for component: Int) -> Float
