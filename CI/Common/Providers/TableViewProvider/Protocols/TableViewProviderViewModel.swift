7c3a0eee56bd855531a9cd64b9f7831b9792c7c4
Yaroslav Abaturov <yarizzer@gmail.com>
2023-09-10 08:18:08 +0200

    TableViewProviderViewModel ch

--- CI/Common/Providers/TableViewProvider/Protocols/TableViewProviderViewModel.swift
+++ CI/Common/Providers/TableViewProvider/Protocols/TableViewProviderViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by Yaroslav Abaturov on 26.07.2023.
 //
 
-protocol TableViewProviderViewModel {
+protocol TableViewProviderViewModel: AnyObject {
     var numberOfTableSections: Int { get }
     func numberOfTableRowsInSection(_ section: Int) -> Int
     func heightForRow(atIndex index: Int) -> Float
