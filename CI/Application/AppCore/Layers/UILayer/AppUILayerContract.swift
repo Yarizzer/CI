27a137f042afec25aa3ad1338a8ab40de17cf0cb
Yaroslav Abaturov <yarizzer@gmail.com>
2023-09-10 08:05:03 +0200

    AppUILayerType ch

--- CI/Application/AppCore/Layers/UILayer/AppUILayerContract.swift
+++ CI/Application/AppCore/Layers/UILayer/AppUILayerContract.swift
@@ -5,7 +5,7 @@
 //  Created by Yaroslav Abaturov on 26.07.2023.
 //
 
-protocol AppUILayerType {
+protocol AppUILayerType: AnyObject {
     var style: AppUILayerStyleWorkerType { get }
     var device: AppUILayerDeviceWorkerType { get }
 }
